e732223a7be26ff38a27e5ab9b5316a2ece07399
Francisco Souza <francisceioseph@gmail.com>
2020-01-27 15:07:08 -0300

    Add new plant repository

--- lib/src/repositories/plant_repository.dart
+++ lib/src/repositories/plant_repository.dart
@@ -1,15 +1,15 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
-const kBaseUrl = 'https://trefle.io/api';
+final kBaseUrl = DotEnv().env['API_HOST'];
 final kApiToken = DotEnv().env['API_TOKEN'];
 
 class PlantRepository {
-  static Future<http.Response> getPlants() {
-    return http.get('$kBaseUrl/plants?token=$kApiToken&page_size=30&page=1');
+  static Future<http.Response> getPlants({int page = 1}) {
+    return http.get('$kBaseUrl/species.json?page=$page');
   }
 
   static Future<http.Response> getPlant(int id) {
-    return http.get('$kBaseUrl/plants/$id?token=$kApiToken');
+    return http.get('$kBaseUrl/species/$id.json');
   }
 }
